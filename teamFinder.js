7522ef9d8b59e383c799b5cb98d469d283ead1ff
Fernando Rojas <75703979+Fernando0422@users.noreply.github.com>
2022-11-28 20:45:04 -0800

    Add files via upload

--- teamFinder.js
+++ teamFinder.js
@@ -15,14 +15,17 @@ function formSubmit() {
     var name = document.getElementById("name").value;
     var email = document.getElementById("email").value;
     var major = document.getElementById("major").value;
+    var skills = document.getElementById('skills').value;
     var suggestions = document.getElementById("suggestions");
     var nameOutput = document.getElementById("nameOutput");
     var emailOutput = document.getElementById("emailOutput");
     var majorOutput = document.getElementById("majorOutput");
-    var skills = document.getElementById("skillsOutput");
+    var skillsOutput = document.getElementById('skillsOutput');
     nameOutput.innerHTML = "Name: " + name;
     emailOutput.innerHTML = "Email: " + email;
     majorOutput.innerHTML = "Major: " + major;
+    skillsOutput.innerHTML = "Skills: " + skills;
+
     suggestions.innerHTML = "Suggestions: ";
     // document.getElementById("selectedSkills").style.display = "block";
     skills.innerHTML =  "Skills: " + skillsOutput;
@@ -34,23 +37,23 @@ function showForm(){
     document.getElementById("formButton").style.display = "none";
 }
 
-function addSkill(skill){
+// function addSkill(skill){
 
-    if(document.getElementById(skill).checked){
-        skillsOutput += skill + ", ";
-    }
-    else{
-        skillsOutput = skillsOutput.replace(skill + ", ", "");
-    }
-    // //only show the skills if there are any
-    // if(skillsOutput.innerHTML == "Skills: "){
-    //     document.getElementById("selectedSkills").style.display="none";
-    // }
-    // else{
-    //     document.getElementById("selectedSkills").style.display="block";
-    // }
+//     if(document.getElementById(skill).checked){
+//         skillsOutput += skill + ", ";
+//     }
+//     else{
+//         skillsOutput = skillsOutput.replace(skill + ", ", "");
+//     }
+//     // //only show the skills if there are any
+//     // if(skillsOutput.innerHTML == "Skills: "){
+//     //     document.getElementById("selectedSkills").style.display="none";
+//     // }
+//     // else{
+//     //     document.getElementById("selectedSkills").style.display="block";
+//     // }
 
-    //only update innerHTML when the form is submitted
+//     //only update innerHTML when the form is submitted
     
 
-}
+// }
